6a69feccb84f3724a2cfcfbd4cebe9c61b14c814
sojin park <psjin977@naver.com>
2023-05-10 18:37:41 +0900

    reset_password

--- MyAnimal/settings.py
+++ MyAnimal/settings.py
@@ -180,7 +180,7 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
 EMAIL_HOST = 'smtp.naver.com' 		 # 메일 호스트 서버
 EMAIL_PORT = '587' 			 # 서버 포트
-EMAIL_HOST_USER = 'yo_yo620@naver.com' 	 # 우리가 사용할 Gmail
-EMAIL_HOST_PASSWORD = 'flvmf1231**'		 # 우리가 사용할 Gmail p
+EMAIL_HOST_USER = environ.get("EMAIL_HOST_USER")  # 발신할 이메일
+EMAIL_HOST_PASSWORD = environ.get("EMAIL_HOST_PASSWORD")  # 발신할 메일의 비밀번호
 EMAIL_USE_TLS = True			 # TLS 보안 설정
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
